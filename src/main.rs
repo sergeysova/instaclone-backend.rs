121f27ecb6d4d8c2a4f438912edf9801769ca40c
Sergey Sova <mail@sergeysova.com>
2018-02-02 01:22:30 +0300

    Add hyper + serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+extern crate hyper;
+extern crate futures;
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+
+use futures::future::Future;
+
+use hyper::header::{ContentLength};
+use hyper::server::{Http, Request, Response, Service};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct HelloResponse {
+    name: String,
+}
+
+impl HelloResponse {
+    fn new(name: String) -> HelloResponse {
+        HelloResponse { name }
+    }
+}
+
+
+struct HelloWorld;
+
+impl Service for HelloWorld {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+    fn call(&self, _req: Request) -> Self::Future {
+        let resp = HelloResponse::new("Sova".into());
+        let resp_string = serde_json::to_string(&resp).unwrap();
+
+        Box::new(futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(resp_string.len() as u64))
+                .with_body(resp_string)
+        ))
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+
+    println!("Listening {address}...", address=addr);
+    server.run().unwrap();
 }
