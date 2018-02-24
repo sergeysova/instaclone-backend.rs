b3e6d9fea6b80ddba87591636923374d418e6d4f
Sergey Sova <mail@sergeysova.com>
2018-02-25 01:07:14 +0300

    start from rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello world"
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
 }
