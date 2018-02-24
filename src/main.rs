7dbfb9f0dc5ea7d94307a17b33e19f04aaaa94fe
Sergey Sova <mail@sergeysova.com>
2018-02-25 01:48:01 +0300

    feat:

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
 
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+extern crate rocket_contrib;
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello world"
-}
 
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, get_user])
+        // .catch(errors![error_404])
+        .launch();
 }
